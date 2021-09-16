e04fe831a4373598a771a5ca953f3e898d80cb62
Shubham Agarwal <shubhamagarwal.alld@gmail.com>
2021-09-16 20:21:44 +0530

    Initial Commit Shubham

---
+++ my_app/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Departments  from './Departments';
+
+//props mein id aayegi required hospital ki..
+//then search for that particular hospital and display details 
+
+function App() {
+  return (
+    <div>
+      <h1>xyz Hospital</h1>
+      <div>
+        <Departments/>
+      </div>
+      <div>
+        <h1>I am doctors component</h1>
+        {/* doctor component*/}
+      </div>
+      <div>
+        <h1>I am rating component</h1>
+        {/* rating component*/}
+      </div>
+      <div>
+        <h1>I am amenities component</h1>
+        {/* amenities component*/}
+      </div>
+      <div>
+        <h1>I am gallery component</h1>
+        {/* gallery component*/}
+      </div>
+      <div>
+        <h1>I am number of beds available component</h1>
+        {/* available number of beds component*/}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my_app/src/Departments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Departments() {
+    return (
+        <div>
+            <h1>Department component</h1>
+        </div>
+    )
+}
+
+export default Departments
