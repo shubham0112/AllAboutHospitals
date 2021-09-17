e5351938ba79ef87b221bd46ac9a19e1dd269174
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2021-09-17 18:15:42 +0530

    app.js conflict

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Hospital  from './components/hospital/Hospital';
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Navbar from './components/Home/Navbar';
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom";
+import Hospital from './components/hospital/Hospital';
 
 function App() {
   return (
     <>
-    <Router>
-     <Navbar/>
-     
-     <Route path='/Home'><Home/></Route>
-    <Route path='/Hospitals'><Hospital id={209480126}/></Route>
-    <Route path='/About'><About/></Route>
-     </Router>
-    
+      <Hospital id={209480126}/>
     </>
   );
 }
