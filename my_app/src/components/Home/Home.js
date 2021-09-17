2bc15d5a23b646a2a78d7e39fa3b4df0b5c0344a
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2021-09-17 15:16:26 +0530

    Home components added

---
+++ my_app/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hospitals from './Hospitals.js'
+import Search from './Search'
+const Home = () => {
+    return (
+        <>
+           
+            <Search/>
+            <Hospitals/>
+        </>
+    )
+}
+
+export default Home
---
+++ my_app/src/components/Home/Search.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <>
+            <h1>Search</h1>
+            <div className="form">
+            <form onSubmit={(e)=>{e.preventDefault();}}>
+                <div className="form-group">
+                <label>Hospital Name:</label>
+                <input type="text"/>
+                </div>
+                <div className="form-group">
+                <label>Disease:</label>
+                <input type="text"/>
+                </div>
+                <div className="form-group">
+                <button type="submit">Search</button>
+                </div>
+            </form>
+            </div>
+           
+        </>
+    )
+}
+
+export default Search
