9520d5ae5463766b99f196591dd83e0d49538206
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2021-10-17 18:57:08 +0530

    merging

--- my_app/src/components/Home/Home.js
+++ my_app/src/components/Home/Home.js
@@ -1,16 +1,32 @@
+// import React from 'react'
+// import Hospitals from './Hospitals.js'
+// import Search from './Search'
+// import '../Home/Home.css'
+// const Home = () => {
+//     return (
+//         <>
+           
+//             <Search/>
+//             <Hospitals/>
+//         </>
+//     )
+// }
+
+// export default Home
+
 import {useState} from 'react'
 import Hospitals from './Hospitals.js'
 import Search from './Search'
 
 import '../Home/Home.css'
-import data from '../../data.js'
+import data from '../../data'
 const Home = () => {
     const [disease,setDisease] =useState('');
     const [hospitalName,setHospitalName] =useState('');
     const [filtered, setFiltered] =useState([]);
     const search=(hospitalName,disease)=>{
         filtered.splice(0,filtered.length)
-        const x=(data.filter(item => item.name===hospitalName));
+        const x=(data.filter(item => item.name===hospitalName&&disease===item.disease));
         // console.log(x);
         setFiltered(x);
         setHospitalName('');
@@ -18,9 +34,9 @@ const Home = () => {
     }
     return (
         <div className="Home">
-           <Search search={search} hospitalName={hospitalName} setHospitalName={setHospitalName} disease={disease} setDisease={setDisease}/> 
+            <Search search={search} hospitalName={hospitalName} setHospitalName={setHospitalName} disease={disease} setDisease={setDisease}/> 
     
-                <Hospitals data={filtered}/>
+            <Hospitals data={filtered}/>
             
         </div>
     )
--- my_app/src/components/Home/Hospitals.js
+++ my_app/src/components/Home/Hospitals.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 
 const Hospitals = ({data}) => {
     const Empty=()=>{
@@ -7,42 +7,34 @@ const Hospitals = ({data}) => {
     }
     const Full=()=>{
         return (
-            
+            <>
+                <div className='card_ka_header_ka_header'>
+                    <div className="card_ka_header">Hospitals</div>
+                </div>
                 <div className="cards">
                     {
-                       data.map(item=><div className="card">
-                          
-                       <h3>{item.name}</h3>
-                       <h3>Contact</h3>
-                       <button>more info</button>
-                   </div>) 
-                      }
-                    </div>
-                
+                        data.map(
+                            item=><div className="card">
+                            
+                                <h3>{item.name}</h3>
+                                <h3>Contact</h3>
+                                <button>
+                                    <Link className='text-link' to={`/Hospital/${data[0].id}`}>
+                                        More info 
+                                    </Link>
+                                </button>
+                            </div>
+                        ) 
+                    }   
+                             
+                </div>
+            </>        
         )
     }
     if(data.length===0)
     return <Empty/>
     else 
     return <Full/>
-  
-    // return (
-    //     <>
-    //     if(data.length==0){
-    //     (<div className="cards"><h1>Empty</h1></div>)
-    // }   else{
-    //       <div className="cards">
-    //           {
-    //              data.map(item=><div className="card">
-                    
-    //              <h3>{item.name}</h3>
-    //              <h3>Contact</h3>
-    //              <button>more info</button>
-    //          </div>) 
-    //             }
-    //           </div>
-    //      }     
-    //     </>
     
 }
 
